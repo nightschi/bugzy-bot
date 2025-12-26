db567b172548c1c7d341a2148e1ec500b28632ae
nightschi <jayjayisyourhomie@gmail.com>
2025-12-25 19:25:04 -0800

    fix: webserver.py

--- webserver.py
+++ webserver.py
@@ -1,15 +1,19 @@
 from flask import Flask
 from threading import Thread
+import os
 
 app = Flask('')
+
 @app.route('/')
 def home():
     return "Running."
 
 def run():
-    port = int(os.environ.get("PORT", 8080))
+    # Render provides a PORT environment variable
+    port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
     t = Thread(target=run)
+    t.daemon = True
     t.start()
