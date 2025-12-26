30c9c99e759e34ee071f07c87146eb6d411b5079
nightschi <jayjayisyourhomie@gmail.com>
2025-12-25 19:17:26 -0800

    fix: flask uptime

--- webserver.py
+++ webserver.py
@@ -2,12 +2,13 @@ from flask import Flask
 from threading import Thread
 
 app = Flask('')
-app.route('/')
+@app.route('/')
 def home():
     return "Running."
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
     t = Thread(target=run)
